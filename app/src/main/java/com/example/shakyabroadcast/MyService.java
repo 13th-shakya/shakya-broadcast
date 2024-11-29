aecd2b64a97406b87035b7376f34d60fa9826200
Riley Ho <riley@rileychh.dev>
2024-11-29 14:20:35 +0800

    feat: add MyService

---
+++ app/src/main/java/com/example/shakyabroadcast/MyService.java
@@ -0,0 +1,47 @@
+package com.example.shakyabroadcast;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    static boolean flag = false;
+
+    private int hour = 0, minute = 0, second = 0;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        flag = intent.getBooleanExtra("flag", false);
+
+        new Thread(() -> {
+            while (flag) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                second++;
+                if (second >= 60) {
+                    second = 0;
+                    minute++;
+                    if (minute >= 60) {
+                        minute = 0;
+                        hour++;
+                    }
+                }
+            }
+
+            Intent i = new Intent("MyMessage");
+            Bundle bundle = new Bundle();
+            bundle.putInt("hour", hour);
+            bundle.putInt("minute", minute);
+            bundle.putInt("second", second);
+            i.putExtras(bundle);
+            sendBroadcast(i);
+        }).start();
+
+        return START_STICKY;
+    }
+}
